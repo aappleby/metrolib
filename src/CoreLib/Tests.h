167c09465a55db27953dbe317937745ffd237e83
Austin Appleby <aappleby@gmail.com>
2023-01-12 20:50:58 -0800

    checkpoint

--- src/CoreLib/Tests.h
+++ src/CoreLib/Tests.h
@@ -74,6 +74,8 @@ struct TestResults {
   int check_pass = 0;
   int check_fail = 0;
 
+  bool ok() const { return (test_fail == 0) && (check_fail == 0); }
+
   void dump() {
     LOG("\r");
     if (test_fail) {
@@ -97,7 +99,7 @@ struct TestResults {
         LOG_G("| ##      ##   ## ####### ####### |  "); LOG_G("\n")
         LOG_G("+---------------------------------+  "); LOG_G("\n");
         if (test_fail == 0 && check_fail == 0) {
-          LOG_G("All tests pass.\n");
+          LOG_G("All tests pass\n");
         }
         LOG_G("\n");
     }
@@ -106,6 +108,7 @@ struct TestResults {
 
 #define TEST_INIT(...) TestResults results; do { LOG("\r"); LOG_B("%s: ", __FUNCTION__); LOG_B("" __VA_ARGS__); LOG_INDENT(); } while(0);
 #define TEST_DONE(...)                      do { if (results.check_fail) { results.test_fail++; LOG("\r"); LOG_R("%s: %d failures\n", __FUNCTION__, results.test_fail); } else { results.test_pass++; LOG_G("" __VA_ARGS__); } LOG("\r"); LOG_DEDENT(); return results; } while(0);
+#define TEST_FAIL(...)                      do { results.test_fail++; LOG_R("" __VA_ARGS__); LOG("\r"); LOG_DEDENT(); return results; } while(0);
 
 #define ASSERT_EQ(A, B, ...) if ((A) == (B)) { results.check_pass++; } else { results.check_fail++; LOG("\r"); LOG_R("ASSERT_EQ fail: %02x != %02x @ %s/%s:%d : ", A, B, __FILE__, __FUNCTION__, __LINE__); LOG_R("" __VA_ARGS__); TEST_DONE(); }
 #define ASSERT_NE(A, B, ...) if ((A) != (B)) { results.check_pass++; } else { results.check_fail++; LOG("\r"); LOG_R("ASSERT_NE fail: %02x == %02x @ %s/%s:%d : ", A, B, __FILE__, __FUNCTION__, __LINE__); LOG_R("" __VA_ARGS__); TEST_DONE(); }
