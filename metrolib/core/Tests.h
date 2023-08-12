5986f3ea102a31db84f37ffa06a62f92089fe829
Austin Appleby <aappleby@gmail.com>
2023-08-11 21:17:41 -0700

    print_want_got broken

--- metrolib/core/Tests.h
+++ metrolib/core/Tests.h
@@ -53,25 +53,28 @@ struct TestResults {
   }
 };
 
+/*
 inline void print_want_got(const void* A, const void* B) { LOG_R("want %p, got %p ", A, B); }
+inline void print_want_got(uint64_t A, uint64_t B)       { LOG_R("want 0x%lx, got 0x%lx ", A, B); }
+inline void print_want_got(uint32_t A, uint32_t B)       { LOG_R("want 0x%x, got 0x%x ", A, B); }
+inline void print_want_got(uint16_t A, uint16_t B)       { LOG_R("want 0x%x, got 0x%x ", A, B); }
+inline void print_want_got(uint8_t A,  uint8_t B)        { LOG_R("want 0x%x, got 0x%x ", A, B); }
 
-inline void print_want_got(uint64_t A, uint64_t B) { LOG_R("want 0x%lx, got 0x%lx ", A, B); }
-//inline void print_want_got(uint32_t A, uint32_t B) { LOG_R("want 0x%x, got 0x%x ", A, B); }
-//inline void print_want_got(uint16_t A, uint16_t B) { LOG_R("want 0x%x, got 0x%x ", A, B); }
-//inline void print_want_got(uint8_t A,  uint8_t B)  { LOG_R("want 0x%x, got 0x%x ", A, B); }
+inline void print_want_got(int64_t A,  int64_t B)        { LOG_R("want %ld, got %ld ", A, B); }
+inline void print_want_got(int32_t A,  int32_t B)        { LOG_R("want %d, got %d ", A, B); }
+inline void print_want_got(int16_t A,  int16_t B)        { LOG_R("want %d, got %d ", A, B); }
+inline void print_want_got(int8_t A,   int8_t B)         { LOG_R("want %d, got %d ", A, B); }
 
-inline void print_want_got(int64_t A,  int64_t B)  { LOG_R("want %ld, got %ld ", A, B); }
-//inline void print_want_got(int32_t A,  int32_t B)  { LOG_R("want %d, got %d ", A, B); }
-//inline void print_want_got(int16_t A,  int16_t B)  { LOG_R("want %d, got %d ", A, B); }
-//inline void print_want_got(int8_t A,   int8_t B)   { LOG_R("want %d, got %d ", A, B); }
-
-inline void print_want_got(double A,   double B)   { LOG_R("want %f, got %f ", A, B); }
-//inline void print_want_got(float A,    float B)    { LOG_R("want %f, got %f ", A, B); }
-inline void print_want_got(bool A,     bool B)     { LOG_R("want %s, got %s ", A ? "true" : "false", B ? "true" : "false"); }
+inline void print_want_got(double A,   double B)         { LOG_R("want %f, got %f ", A, B); }
+inline void print_want_got(float A,    float B)          { LOG_R("want %f, got %f ", A, B); }
+inline void print_want_got(bool A,     bool B)           { LOG_R("want %s, got %s ", A ? "true" : "false", B ? "true" : "false"); }
 
 inline void print_want_got(const std::string& a, const std::string& b) {
   LOG_R("want {%s}, got {%s} ", a.c_str(), b.c_str());
 }
+*/
+template<typename A, typename B>
+inline void print_want_got(const A& a, const B& b) { }
 
 #define TEST_INIT(...) TestResults results; do { LOG("\r"); LOG_B("%s: ", __FUNCTION__); LOG_B("" __VA_ARGS__); LOG_INDENT(); } while(0);
 #define TEST_DONE(...)                      do { if (results.check_fail) { results.test_fail++; LOG("\r"); LOG_R("%s: %d failures\n", __FUNCTION__, results.test_fail); } else { results.test_pass++; LOG_G("" __VA_ARGS__); } LOG("\r"); LOG_DEDENT(); return results; } while(0);
